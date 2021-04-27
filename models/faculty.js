b738e44d8ba0fc0fa7360122ec439b9f3cfaf77f
Vallab Karanam <k.vallab6@gmail.com>
2021-04-26 23:04:53 -0500

    models/faculty.js updated

--- models/faculty.js
+++ models/faculty.js
@@ -2,103 +2,31 @@
 
 const mongoose = require("mongoose");
 
-const publicationSchema = new mongoose.Schema({
-  address: {
+const facultySchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  department: {
     type: String,
     required: false,
   },
-  annote: {
-    type: String,
-    required: false,
-  },
-  author: {
-    type: String,
-    required: false,
-  },
-  booktitle: {
-    type: String,
-    required: false,
-  },
-  chapter: {
-    type: String,
-    required: false,
-  },
-  crossref: {
-    type: String,
-    required: false,
-  },
-  edition: {
-    type: String,
-    required: false,
-  },
-  editor: {
-    type: String,
-    required: false,
-  },
-  howpublished: {
-    type: String,
-    required: false,
-  },
-  institution: {
-    type: String,
-    required: false,
-  },
-  journal: {
-    type: String,
-    required: false,
-  },
-  key: {
-    type: String,
-    required: false,
-  },
-  month: {
-    type: String,
-    required: false,
-  },
-  note: {
-    type: String,
+  email: {
+    type: String, //email type?
     required: false,
   },
   number: {
-    type: String,
+    type: Number, //phone number type?
     required: false,
   },
-  organization: {
-    type: String,
-    required: false,
-  },
-  pages: {
-    type: String,
-    required: false,
-  },
-  publisher: {
-    type: String,
-    required: false,
-  },
-  school: {
-    type: String,
-    required: false,
-  },
-  series: {
-    type: String,
-    required: false,
-  },
-  title: {
-    type: String,
-    required: false,
-  },
-  type: {
-    type: String,
-    required: false,
-  },
-  volume: {
-    type: String,
-    required: false,
-  },
-  year: {
-    type: String,
+  pic: {
+    type: Image,
     required: false,
   },
 });
 
-module.exports = mongoose.model("Publication", publicationSchema);
+module.exports = mongoose.model("Faculty", facultySchema);
