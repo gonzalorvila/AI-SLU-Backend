fdcad1a805cf6cc2eed3c0870dfb9e89fccaef34
Vallab Karanam <k.vallab6@gmail.com>
2021-04-28 15:20:59 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer');
+var express = require("express");
+var bodyParser = require("body-parser");
+var multer = require("multer");
 var upload = multer();
-var database = require('./db');
-
-
+var database = require("./db");
 
 var app = express();
-app.use(express.json())
+app.use(express.json());
 
-const publicationsRouter = require('./routes/publications')
-app.use('/publications', publicationsRouter)
+const publicationsRouter = require("./routes/publications");
+app.use("/publications", publicationsRouter);
 
-app.listen(3000, () => console.log('server started'))
+const facultyRouter = require("./routes/faculty");
+app.use("/faculty", facultyRouter);
+
+app.listen(3000, () => console.log("server started"));
